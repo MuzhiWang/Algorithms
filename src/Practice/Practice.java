9d2c275d044fafae7b3e96810a0f87ac78c8098c
MuzhiWang <wmz669082@gmail.com>
2015-01-03 21:52:23 -0600

    new start

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -31,5 +31,40 @@ public class Practice {
         });
         
     }
+   
+    /*******  Array Hopper IV  *******/
+    public int minJump(int[] array, int index) {
+      if (array == null || array.length == 0)
+        return -1;
+      if ((array.length == 1 && array[0] == 0))
+        return 0;
+      boolean[] reach = new boolean[array.length];
+      int[] min = new int[array.length];
+      for (int i : min) {
+        i = Integer.MAX_VALUE;
+      }
+      if (array[0] != 0)
+        reach[0] = true;
+      for (int i = 1; i < array.length; ++i) {
+        for (int j = 0; j < i; ++j) {
+          if (reach[j] && array[j] >= i - j) {
+            min[i] = Math.min(min[i], min[j] + 1);
+            reach[i] = true;
+          }
+        }
+      }
+      if (reach[array.length - 1] == true)
+        return min[array.length - 1];
+      else
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+        //int index = 
+        int[] array = {};
+        Practice p = new Practice();
+        int min = p.minJump(array, 0);
+        System.out.println(min);
+    }
     
 }
