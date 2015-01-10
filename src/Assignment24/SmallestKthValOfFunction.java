fcc67c71867ba6ee50faa3be9e3878c837f235d7
MuzhiWang <wmz669082@gmail.com>
2015-01-10 13:45:07 -0600

    Jan 10 03

--- src/Assignment24/SmallestKthValOfFunction.java
+++ src/Assignment24/SmallestKthValOfFunction.java
@@ -6,6 +6,8 @@
 package Assignment24;
 
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -26,13 +28,35 @@ public class SmallestKthValOfFunction {
     }
     
     public List<Double> smallestKthVal(int k) {
-        PriorityQueue<Ele> maxHeap = new PriorityQueue<>(k, new Comparator<Ele>(){
+        PriorityQueue<Ele> minHeap = new PriorityQueue<>(new Comparator<Ele>(){
             @Override
             public int compare(Ele a, Ele b) {
                 return (int)(a.f - b.f);
             }
         });
-        
+        HashSet<Ele> hashSet = new HashSet<>();
+        hashSet.add(new Ele(1, 1, 1));
+        List<Double> res = new LinkedList<>();
+        while (res.size() < k) {
+            Ele cur = minHeap.poll();
+            res.add(cur.f);
+            Ele extand1 = new Ele(cur.x + 1, cur.y, cur.z);
+            Ele extand2 = new Ele(cur.x, cur.y + 1, cur.z);
+            Ele extand3 = new Ele(cur.x, cur.y, cur.z + 1);
+            if (!hashSet.contains(extand1))
+                hashSet.add(extand1);
+            if (!hashSet.contains(extand2))
+                hashSet.add(extand2);
+            if (!hashSet.contains(extand3))
+                hashSet.add(extand3);
+        }
+        return res;
+    }
+    
+    public static void main(String[] args) {
+        SmallestKthValOfFunction s = new SmallestKthValOfFunction();
+        for (double b : s.smallestKthVal(5))
+            System.out.print(b + "  ");
         
     }
     
