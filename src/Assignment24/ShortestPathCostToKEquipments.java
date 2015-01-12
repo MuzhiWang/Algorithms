f37a61f584871a179e3916debbe208bc69c4cf34
MuzhiWang <wmz669082@gmail.com>
2015-01-12 13:41:31 -0600

    Jan 12 01

---
+++ src/Assignment24/ShortestPathCostToKEquipments.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment24;
+
+import java.util.List;
+
+/**
+ *
+ * @author Muzhi
+ */
+public class ShortestPathCostToKEquipments {
+    public List<Integer> shortestPath(char[][] gym) {
+        
+        
+        
+    }
+    
+    
+}
