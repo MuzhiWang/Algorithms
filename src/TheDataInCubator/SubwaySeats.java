1aa75501e34379caf47951fc60252e111bdf5797
MuzhiWang <wmz669082@gmail.com>
2015-02-04 00:36:10 -0600

    Feb 3

--- src/TheDataInCubator/SubwaySeats.java
+++ src/TheDataInCubator/SubwaySeats.java
@@ -5,9 +5,6 @@
  */
 package TheDataInCubator;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,21 +15,80 @@ import java.util.List;
 public class SubwaySeats {
     
     public void subway(int N) {
-        assert N > 0;
+        assert N > 2;
         int maxSeats = N / 2;
         int minSeats = N / 3;
         
-        for (int i = minSeats; i < maxSeats; ++i) {
-            
+        // seat[][]: seat[][0] num of seat occupied, seat[][1] combinations of that num of seats occupied.
+        // 1: full seats in N
+        double[][] seatOne = seatsCalculate(N);
+        // 2: ******* X , with last seat occupied.
+        double[][] seatTwo = seatsCalculate(N - 1);
+        // 3. O ********, with first seat empty & full seats in N - 1.
+        double[][] seatThree = seatsCalculate(N - 1);
+        // 4. O ******** X, with first seat empty & last seat occupied.
+        double[][] seatFour = seatsCalculate(N - 2);
+        
+        double mean = 0;
+        int num = 0;
+        for (int i = 0; i < seatOne.length; ++i) {
+            if (seatOne[i][0] > 0) {
+                num += seatOne[i][1];
+                mean += seatOne[i][0] * seatOne[i][1];
+            }
         }
+        for (int i = 0; i < seatTwo.length; ++i) {
+            if (seatTwo[i][0] > 0) {
+                num += seatTwo[i][1];
+                mean += (seatTwo[i][0] + 1) * seatTwo[i][1];
+            }
+        }
+        for (int i = 0; i < seatThree.length; ++i) {
+            if (seatThree[i][0] > 0) {
+                num += seatThree[i][1];
+                mean += seatThree[i][0] * seatThree[i][1];
+            }
+        }
+        for (int i = 0; i < seatFour.length; ++i) {
+            if (seatFour[i][0] > 0) {
+                num += seatFour[i][1];
+                mean += (seatFour[i][0] + 1) * seatFour[i][1];
+            }
+        }
+        mean = mean / num;
+        meanRes = mean;
+        
+        double sd = 0;
+        for (int i = 0; i < seatOne.length; ++i) {
+            if (seatOne[i][0] > 0) {
+                sd = (seatOne[i][0] - mean) * (seatOne[i][0] - mean) * seatOne[i][1];
+            }
+        }
+        for (int i = 0; i < seatTwo.length; ++i) {
+            if (seatTwo[i][0] > 0) {
+                sd = (seatTwo[i][0] + 1 - mean) * (seatTwo[i][0] + 1 - mean) * seatTwo[i][1];
+            }
+        }
+        for (int i = 0; i < seatThree.length; ++i) {
+            if (seatThree[i][0] > 0) {
+                sd = (seatThree[i][0] - mean) * (seatThree[i][0] - mean) * seatThree[i][1];
+            }
+        }
+        for (int i = 0; i < seatFour.length; ++i) {
+            if (seatFour[i][0] > 0) {
+                sd = (seatFour[i][0] + 1 - mean) * (seatFour[i][0] + 1 - mean) * seatFour[i][1];
+            }
+        }
+        sd = Math.sqrt(sd / num);
+        standardDeviationRes = sd;
         
     }
     
     private double[][] seatsCalculate(int n) {
         int maxSeats = n / 2;
         int minSeats = n / 3;
-        HashMap<Integer, Double> hashMap = new HashMap<>();
-        double[][] res = new double[maxSeats - minSeats][2];
+        //HashMap<Integer, Double> hashMap = new HashMap<>();
+        double[][] res = new double[maxSeats - minSeats + 1][2];
         for (int i = minSeats; i <= maxSeats; ++i) {
             for (int j = 0; j <= i; ++j) {
                 if (j * 2 + (i - j) * 3 == n) {
@@ -60,7 +116,7 @@ public class SubwaySeats {
     
     
     /*******  Recursion method, but memory overflowed when N > 50  ********/
-    public int meanRes = 0;
+    public double meanRes = 0;
     public double standardDeviationRes = 0;
     public void subwaySeats(int N) {
         assert N >= 1;
@@ -107,14 +163,18 @@ public class SubwaySeats {
     
     public static void main(String[] args) {
         SubwaySeats subwaySeat = new SubwaySeats();
-        subwaySeat.subwaySeats(25);
+        subwaySeat.subway(25);
         System.out.println("25 seats's mean num: " + subwaySeat.meanRes);
         System.out.println("25 seats's standard deviation: " + subwaySeat.standardDeviationRes);
         
-        subwaySeat.subwaySeats(50);
-        System.out.println("50000 seats's mean num: " + subwaySeat.meanRes);
-        System.out.println("50000 seats's standard deviation: " + subwaySeat.standardDeviationRes);
+        int num = 50000
+                ;
+        subwaySeat.subway(num);
+        System.out.println(num + " seats's mean num: " + subwaySeat.meanRes);
+        System.out.println(num + " seats's standard deviation: " + subwaySeat.standardDeviationRes);
         
+        double test = subwaySeat.combination(6, 2);
+        System.out.println(test);
     }
     
 }
