60aed19ec4ef0207c2aa5538fc7d771c9840309f
MuzhiWang <wmz669082@gmail.com>
2015-03-03 14:46:58 -0600

    March 3 05

---
+++ src/Epic/AdvisedAverage.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Epic;
+
+/**
+ *
+ * @author Muzhi
+ */
+
+/*	Write a program to display the advised average for the list of 
+ *	numbers omitting the largest three number in the series.
+ *	e.g. 3,6,12,55,289, 600, 534, 900, 172, 
+ *	avg = (3+6+12+55+289+172)/6 and eliminating 534, 900, 600;
+ */
+
+public class AdvisedAverage {
+    public int average() {
+        
+        return -1;
+    }
+}
--- src/Epic/ArrayIntoTriangle.java
+++ src/Epic/ArrayIntoTriangle.java
@@ -5,10 +5,49 @@
  */
 package Epic;
 
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  *
  * @author Muzhi
  */
+
+/*	Given a array, construct a triangle;
+*
+*	Example:
+*	input {4,7,3,6,7};
+*	output
+*	{81}
+*	{40,41}
+*	{21,19,22}
+*	{11,10,9,13}
+*	{4,7,3,6,7}
+*/
+
 public class ArrayIntoTriangle {
+    public void triangle(int[] array) {
+        assert array == null && array.length > 0;
+        int[] out = array;
+        Deque<int[]> stack = new LinkedList<>();
+        while (out.length > 0) {
+            //System.out.println(Arrays.toString(out));
+            stack.offerFirst(out);
+            int[] temp = new int[out.length - 1];
+            for (int i = 0; i < temp.length; ++i) {
+                temp[i] = out[i] + out[i + 1];
+            }
+            out = temp;
+        }
+        while (!stack.isEmpty()) {
+            System.out.println(Arrays.toString(stack.poll()));
+        }
+    }
+    
+    public static void main(String... args) {
+        ArrayIntoTriangle triangle = new ArrayIntoTriangle();
+        triangle.triangle(new int[]{4, 7, 3, 6, 7});
+    }
     
 }
