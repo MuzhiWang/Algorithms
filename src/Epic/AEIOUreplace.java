5a55a59d3a431b67306128cf7c92598f7191bbf2
MuzhiWang <wmz669082@gmail.com>
2015-03-03 13:40:50 -0600

    March 3 03

--- src/Epic/AEIOUreplace.java
+++ src/Epic/AEIOUreplace.java
@@ -9,8 +9,34 @@ package Epic;
  *
  * @author Muzhi
  */
+
+/*	Replace a e i o u, with A E I O U.
+ *	At most four eligible letters from the rear of this string are
+ *	replaced. The first three eligible letters in the string are 
+ *	always exempted from replacement;
+ *
+ */
+
 public class AEIOUreplace {
+    public String aeiouReplace(String str) {
+        assert str == null || str.length() > 0;
+        int count = 0;
+        char[] strChar = str.toCharArray();
+        for (int i = 0; i < strChar.length; ++i) {
+            if (strChar[i] == 'a' || strChar[i] == 'e' || strChar[i] == 'i' || strChar[i] == 'o'
+                    || strChar[i] == 'u') {
+                count++;
+                if (count > 3)
+                    strChar[i] = (char)(strChar[i] - 'a' + 'A');
+            }
+        }
+        return new String(strChar);
+    }
     
-    
+    public static void main(String... args) {
+        AEIOUreplace ae = new AEIOUreplace();
+        String str = "an apple a morning keeps me yawning";
+        System.out.println(ae.aeiouReplace(str));
+    }
     
 }
